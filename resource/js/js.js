932e1674b1af9152a376695a08696559bb70b8c0
Hossein Arjomandi <hma.globe@gmail.com>
2021-01-21 13:19:16 +0330

    Onclicks implemented

---
+++ resource/js/js.js
@@ -0,0 +1,19 @@
+function login()
+{
+    window.open('login.html', '_self');
+}
+
+function doctors()
+{
+    window.open('doctors.html', '_self');
+}
+
+function doctor_instance()
+{
+    window.open('doctor291.html', '_self');
+}
+
+function specialities()
+{
+    window.open('medical-specialities.html', '_self');
+}
