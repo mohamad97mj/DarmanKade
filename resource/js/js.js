73bd8704254f2bd5be6a8750f6e021fd78874933
mohamad <mohamad97mj@gmail.com>
2021-01-24 11:37:16 +0330

    some bugs fixed

--- resource/js/js.js
+++ resource/js/js.js
@@ -21,3 +21,18 @@ function specialities()
 const redirect = function (url) {
     location.href = url;
 };
+
+const myfetch = async (url2fetch) => {
+    try {
+        let response = await fetch(url2fetch);
+        // console.log(response)
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const addElementBeforeEnd = (targetId, html2insert) => {
+    let parent = document.getElementById(targetId);
+    parent.insertAdjacentHTML('beforeend', html2insert);
+}
